326008a1e7e08baf95145fa67f40fe2a04cb15d9
Ranjith <ranjithkumar13579ark@gmail.com>
2026-07-21 12:04:36 +0530

    add responsive

--- src/Components/BookFuel/Order/Order.jsx
+++ src/Components/BookFuel/Order/Order.jsx
@@ -168,7 +168,7 @@ const navigate = useNavigate();
 
     <div className='d-flex justify-content-between align-items-center'>
       <h6 className='mb-0'>Total</h6>
-      <h5 className='mb-0'>₹{totalprice}</h5>
+      <h5 className='text-primary mb-0'>₹{totalprice}</h5>
     </div>
 
     <button 
